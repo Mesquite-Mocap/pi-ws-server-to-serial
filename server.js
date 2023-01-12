5b7ffe4be68decf5ebf34b4cefbb3eac84bcedd1
Pavan Krishna <pavankrishna@Pavans-MacBook-Pro.local>
2023-01-11 22:32:37 -0700

    testing

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 /* This is the websocket running on the r pi that's listening for messages from the router */
 
-var SerialPort = require('serialport'); // we are going to push this data via serial
-var port = new SerialPort('/dev/ttyGS0', { //update your port
-   baudRate: 115200,
-   dataBits: 8,
-   parity: 'none',
-   stopBits: 1,
-   flowControl: false
-});
-
-
-function write(str){
-	port.write(str + '\n',function(err){ // the string being pushed
-		if(err){
-			console.log("e");
-		}
-	})
-}
+// var SerialPort = require('serialport'); // we are going to push this data via serial
+// var port = new SerialPort('/dev/ttyGS0', { //update your port
+//    baudRate: 115200,
+//    dataBits: 8,
+//    parity: 'none',
+//    stopBits: 1,
+//    flowControl: false
+// });
+//
+//
+// function write(str){
+// 	port.write(str + '\n',function(err){ // the string being pushed
+// 		if(err){
+// 			console.log("e");
+// 		}
+// 	})
+// }
 
 const WebSocket = require('ws')
 
