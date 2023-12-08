fbc1d0d7d44913661fa9be168b2ab5f4da05d92a
Tejaswi Gowda <tejaswi@asu.edu>
2023-12-08 15:35:34 -0700

    ac

---
+++ public/face.js
@@ -0,0 +1,26 @@
+import vision from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
+const { FaceLandmarker, FilesetResolver, DrawingUtils } = vision;
+
+let faceLandmarker;
+let runningMode = "VIDEO"
+let webcamRunning  = false;
+const videoWidth = 480;
+
+
+async function createFaceLandmarker() {
+    const filesetResolver = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+    );
+    faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+      baseOptions: {
+        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+        delegate: "GPU"
+      },
+      outputFaceBlendshapes: true,
+      runningMode,
+      numFaces: 1
+    });
+  }
+  createFaceLandmarker();
+
+  
